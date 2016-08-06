aeff22c0e77d629e2abc28d130ad1ea85c3a30bc
杉山佑里子 <peaceful.future1101@gmail.com>
2016-08-06 19:48:42 +0900

    initial commit

---
+++ Config/Migration/1470479519_create_users_table.php
@@ -0,0 +1,68 @@
+<?php
+class CreateUsersTable extends CakeMigration {
+
+	/**
+	 * Migration description
+	 *
+	 * @var string
+	 */
+	public $description = 'create_users_table';
+
+	/**
+	 * Actions to be performed
+	 *
+	 * @var array $migration
+	 */
+	public $migration = array(
+
+		'up'            => array(
+			'create_table' => array(
+				'users'       => array(
+					'id'         => array(
+						'type'      => 'integer',
+						'null'      => false,
+						'default'   => null,
+						'length'    => 36,
+						'key'       => 'primary',
+					),
+					'username' => array(
+						'type'    => 'string',
+						'null'    => false,
+						'default' => null
+					),
+					'password' => array(
+						'type'    => 'string',
+						'null'    => false,
+						'default' => null
+					),
+				),
+			),
+		),
+
+		'down'        => array(
+			'drop_table' => array(
+				'users',
+			)
+		),
+	);
+
+	/**
+	 * Before migration callback
+	 *
+	 * @param string $direction Direction of migration process (up or down)
+	 * @return bool Should process continue
+	 */
+	public function before($direction) {
+		return true;
+	}
+
+	/**
+	 * After migration callback
+	 *
+	 * @param string $direction Direction of migration process (up or down)
+	 * @return bool Should process continue
+	 */
+	public function after($direction) {
+		return true;
+	}
+}
---
+++ Model/User.php
@@ -0,0 +1,36 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * User Model
+ *
+ */
+class User extends AppModel {
+
+/**
+ * Validation rules
+ *
+ * @var array
+ */
+	public $validate = array(
+		'username' => array(
+			'notBlank' => array(
+				'rule' => array('notBlank'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'password' => array(
+			'notBlank' => array(
+				'rule' => array('notBlank'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+	);
+}
