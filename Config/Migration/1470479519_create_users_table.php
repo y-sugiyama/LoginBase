e17fbe8fd7a47f4635e4bf3fb98289b587f44996
杉山佑里子 <peaceful.future1101@gmail.com>
2016-08-20 19:46:32 +0900

    afterメソッドでuserテーブル情報追加､nbprojectの削除

--- Config/Migration/1470479519_create_users_table.php
+++ Config/Migration/1470479519_create_users_table.php
@@ -1,73 +1,88 @@
 <?php
+App::uses('User', 'Model');
+
 class CreateUsersTable extends CakeMigration {
 
-	/**
-	 * Migration description
-	 *
-	 * @var string
-	 */
-	public $description = 'create_users_table';
+    /**
+     * Migration description
+     *
+     * @var string
+     */
+    public $description = 'create_users_table';
 
-	/**
-	 * Actions to be performed
-	 *
-	 * @var array $migration
-	 */
-	public $migration = array(
+    /**
+     * Actions to be performed
+     *
+     * @var array $migration
+     */
+    public $migration = array(
+        'up' => array(
+            'create_table' => array(
+                'users' => array(
+                    'id' => array(
+                        'type' => 'integer',
+                        'null' => false,
+                        'default' => null,
+                        'length' => 36,
+                        'key' => 'primary',
+                    ),
+                    'username' => array(
+                        'type' => 'string',
+                        'null' => false,
+                        'default' => null
+                    ),
+                    'password' => array(
+                        'type' => 'string',
+                        'null' => false,
+                        'default' => null
+                    ),
+                    'role' => array(
+                        'type' => 'string',
+                        'null' => false,
+                        'default' => null
+                    ),
+                ),
+            ),
+        ),
+        'down' => array(
+            'drop_table' => array(
+                'users',
+            )
+        ),
+    );
 
-		'up'            => array(
-			'create_table' => array(
-				'users'       => array(
-					'id'         => array(
-						'type'      => 'integer',
-						'null'      => false,
-						'default'   => null,
-						'length'    => 36,
-						'key'       => 'primary',
-					),
-					'username' => array(
-						'type'    => 'string',
-						'null'    => false,
-						'default' => null
-					),
-					'password' => array(
-						'type'    => 'string',
-						'null'    => false,
-						'default' => null
-					),
-                                        'role' => array(
-						'type'    => 'string',
-						'null'    => false,
-						'default' => null
-					),
-				),
-			),
-		),
+    /**
+     * Before migration callback
+     *
+     * @param string $direction Direction of migration process (up or down)
+     * @return bool Should process continue
+     */
+    public function before($direction) {
+        return true;
+    }
 
-		'down'        => array(
-			'drop_table' => array(
-				'users',
-			)
-		),
-	);
+    /**
+     * After migration callback
+     *
+     * @param string $direction Direction of migration process (up or down)
+     * @return bool Should process continue
+     */
+    public function after($direction) {
+        //もしupだったら
+        if ($direction === 'up') {
+            //新しくUserクラスのインスタンスをつくる 初期データをつくる
+            $user = new User();
+            //databaseの初期値の設定に基づいて､インスタンスを初期化する
+            $user->create();
+            //以下の値で上書きしてdatabaseに保存する
+            $user->save([
+                'username' => 'admin',
+                'password' => 'admin',
+                'role' => 'admin'
+            ]);
+        }
+//         @return bool Should process continue
+        return true;
+    }
 
-	/**
-	 * Before migration callback
-	 *
-	 * @param string $direction Direction of migration process (up or down)
-	 * @return bool Should process continue
-	 */
-	public function before($direction) {
-		return true;
-	}
-
-	/**
-	 * After migration callback
-	 *
-	 * @param string $direction Direction of migration process (up or down)
-	 * @return bool Should process continue
-	 */
-	public function after($direction) {
-		return true;
-	}
 }
